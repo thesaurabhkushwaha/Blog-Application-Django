e7ceafb038231c944a883a524027db816dc48c72
Saurabh Kushwaha <saurabh.sprk@gmail.com>
2020-10-05 18:49:10 +0530

    Updates settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 EMAIL_HOST  =   'smtp.gmail.com'
-EMAIL_HOST_USER =   'saurabhkratos619@gmail.com'
-EMAIL_HOST_PASSWORD =   '1999@BLazer1999'
+EMAIL_HOST_USER =  os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD =   os.environ.get('EMAIL_PASS')
 EMAIL_PORT  =   587
 EMAIL_USE_TLS   =   True
