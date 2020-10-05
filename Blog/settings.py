5ea6588550900de5956dd10073020ababad7b556
Saurabh Kushwaha <saurabh.sprk@gmail.com>
2020-10-05 17:08:15 +0530

    Initial commit

--- Blog/settings.py
+++ Blog/settings.py
@@ -129,11 +129,3 @@ EMAIL_PORT  =   587
 EMAIL_USE_TLS   =   True
 
 
-from decouple import config
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-
-
-from dj_database_url import parse as dburl
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
